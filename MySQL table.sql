73ba533bb01952931addfa90ed93f29899a8565a
Anirban <134738334+Anirban-Gh0sh@users.noreply.github.com>
2023-06-30 12:52:59 +0530

    Create MySQL table.sql

---
+++ MySQL table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE college_students (
+    reg_no VARCHAR(10) PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    city VARCHAR(50) NOT NULL
+);
